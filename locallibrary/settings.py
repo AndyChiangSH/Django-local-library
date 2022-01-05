001145dbb9a11df98f41ceb3442b386d934e893d
Andy Chiang <andy10801@gmail.com>
2022-01-05 12:16:24 +0800

    set new config: SECRET_KEY and DEBUG

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'mhg-qmwta^7k(7i&+)fb7ryv$zd#_p
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['young-fortress-55289.herokuapp.com']
+ALLOWED_HOSTS = ['young-fortress-55289.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
